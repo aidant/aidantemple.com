6f70b55591228448b9fd1c2e4758d79154f1ca63
aidant <15520814+aidant@users.noreply.github.com>
2023-04-25 18:33:55 +1000

    add security headers

---
+++ src/hooks.server.ts
@@ -0,0 +1,12 @@
+export async function handle({ event, resolve }) {
+  const response = await resolve(event)
+
+  response.headers.set('Content-Security-Policy', "default-src 'self'")
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN')
+  response.headers.set('X-XSS-Protection', '1; mode=block')
+
+  return response
+}
