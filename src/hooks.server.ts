61e245fc6899c0cac3adade89f9287de344d2d94
aidant <15520814+aidant@users.noreply.github.com>
2023-04-25 19:19:46 +1000

    update csp

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,9 @@
 export async function handle({ event, resolve }) {
   const response = await resolve(event)
 
+  if (!response.headers.has('Content-Security-Policy')) {
+    response.headers.set('Content-Security-Policy', "default-src 'self'")
+  }
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
   response.headers.set('Strict-Transport-Security', 'max-age=31536000')
   response.headers.set('X-Content-Type-Options', 'nosniff')
