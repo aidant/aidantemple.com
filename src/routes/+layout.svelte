8ec2812e620c7abdd90edaa0cd3cb5bfa6004f5e
aidant <15520814+aidant@users.noreply.github.com>
2023-05-21 10:30:24 +1000

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+  import { pointer } from '$lib/actions/pointer'
   import '../app.css'
 </script>
+
+<div
+  use:pointer
+  class="absolute left-[calc(var(--pointer-x)*50vw+50vw)] top-[calc(var(--pointer-y)*50vh+50vh)] h-4 w-4 rounded-full bg-black"
+/>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
-<main class="m-24 flex flex-row items-center justify-around border border-black border-opacity-50">
-  <h1 class="font-serif text-9xl font-normal [font-variation-settings:'WONK'_0]">
+<main class="m-24 flex flex-row items-center justify-around border-8 border-black p-24">
+  <h1
+    class="font-serif text-[16rem] font-semibold [font-variation-settings:'WONK'_0] [line-height:1]"
+  >
     Aidan
     <br />
     Temple
   </h1>
   <img
-    class="w-1/3 grayscale"
-    src="./DSC01425.JPG"
+    class="w-1/4 grayscale"
+    src="./aidan-temple.jpg"
     alt=""
   />
 </main>
